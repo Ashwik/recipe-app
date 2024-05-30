25ef256af00f7bcc8d050bab50135a54bbe8aea4
Ashwik <ashwikreddyaileni999@gmail.com>
2024-05-30 18:58:13 +0530

    create tag api

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -1,9 +1,14 @@
 """
 Views for the recipe app
 """
-from rest_framework import viewsets, authentication, permissions
+from rest_framework import (
+    viewsets,
+    authentication,
+    permissions,
+    mixins
+)
 
-from core.models import Recipe
+from core.models import Recipe, Tag
 from recipe import serializers
 
 
@@ -28,3 +33,17 @@ class RecipeViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         """Create a new recipe"""
         serializer.save(user=self.request.user)
+
+
+class TagViewSet(mixins.DestroyModelMixin,
+                 mixins.UpdateModelMixin,
+                 mixins.ListModelMixin, viewsets.GenericViewSet):
+    """View for manage tag APIS"""
+    serializer_class = serializers.TagSerializer
+    queryset = Tag.objects.all()
+    authentication_classes = (authentication.TokenAuthentication,)
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def get_queryset(self):
+        """Return objects for the current authenticated user only"""
+        return self.queryset.filter(user=self.request.user).order_by('-name')
